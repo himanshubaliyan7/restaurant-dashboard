b68bee37402008992bee057a881258a73da04669
Himanshu Baliyan <himanshubaliyan4000@gmail.com>
2025-01-01 18:06:47 +0530

    Backend-Removed

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const categoryRoutes = require("./categoryRoutes");
-const itemRoutes = require("./itemRoutes");
-
-router.use("/categories", categoryRoutes);
-router.use("/items", itemRoutes);
-
-module.exports = router;
--- backend/routes/itemRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createItem, getAllItems } = require("../controllers/itemController");
-
-router.post("/", createItem);
-router.get("/", getAllItems);
-
-module.exports = router;
